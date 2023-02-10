55c3e7afa0e427bc3107055ad699a9b5209380c9
MelvinKim <melobruk1@gmail.com>
2023-02-10 21:13:51 +0300

    feat: registering routes

--- Cmd/main.go
+++ Cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	router "github.com/MelvinKim/Design-Reddit-API/http"
+)
+
+var (
+	httpRouter router.Router = router.NewChiRouter()
+)
+
 func main() {
+	const port string = ":8080"
+
+	httpRouter.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "server is app and running ....")
+	})
+
+	// TODO: register the route groups here
+
+	httpRouter.SERVE(port)
 
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/MelvinKim/Design-Reddit-API
 go 1.19
 
 require golang.org/x/crypto v0.6.0
+
+require github.com/go-chi/chi/v5 v5.0.8 // indirect
